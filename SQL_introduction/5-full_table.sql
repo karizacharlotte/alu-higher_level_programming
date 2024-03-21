cf3b3022f1b77b4ab76f4057dadc47a7b63c53e1
karizacharlotte <c.kariza@alustudent.com>
2024-03-21 07:32:08 -0700

    hello

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,2 +1,2 @@
---Script to print the full description of the table first_tabl
-SHOW CREATE TABLE first_table:
+-- Script to print the full description of the table first_table 
+SHOW CREATE TABLE first_table;
