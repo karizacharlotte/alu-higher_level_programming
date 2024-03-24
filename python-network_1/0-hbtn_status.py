25db0289cbee256ba5c7f083f732f703a140d5d6
karizacharlotte <c.kariza@alustudent.com>
2024-03-24 15:30:49 +0000

    BEST

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
-"""Import urllib library."""
-
+# fetches https://alu-intranet.hbtn.io/status
 import urllib.request
 
 
-"""fetch the url then."""
-if __name__ == "__main__":
-    with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
-        html = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode("utf-8")))
+with urllib.request.urlopen('https://intranet.hbtn.io/status') as url:
+    s = url.read()
+    req = "Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
+    print(req.format(type(s), s, s.decode('utf-8')))
