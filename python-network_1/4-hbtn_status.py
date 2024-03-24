41aaf4b6677d27e4af8f5b96be6c9206b2c4eaf2
karizacharlotte <c.kariza@alustudent.com>
2024-03-24 16:59:39 +0000

    END

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
+"""
+Python script that fetches an URL with requests package
+"""
 import requests
 
+
 if __name__ == "__main__":
-    url = 'https://intranet.hbtn.io/status'
-    response = requests.get(url)
-    content_type = type(response.text)
-    content = response.text.strip()
-    
-    print("Body response:")
-    print("\t- type: {}".format(content_type))
-    print("\t- content: {}".format(content))
+    r = requests.get('https://intranet.hbtn.io/status')
+    t = r.text
+    print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(t), t))
