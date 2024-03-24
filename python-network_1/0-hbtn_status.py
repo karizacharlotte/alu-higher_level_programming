d2e7f1ae4346e3408414469cd8034a5038c487c9
karizacharlotte <c.kariza@alustudent.com>
2024-03-24 17:12:31 +0000

    BEST

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,12 +1,12 @@
-#!/usr/bin/python3
-""" Pythonn script that fetches https://intranet.hbtn.io/status """
 import urllib.request
+url = 'https://alu-intranet.hbtn.io/status'
 
-
-if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        html = response.read()
-        print('Body response:')
-        print('\t- type: {}'.format(type(html)))
-        print('\t- content: {}'.format(html))
-        print('\t- utf8 content: {}'.format(html.decode("utf-8")))
+try:
+    with urllib.request.urlopen(url) as response:
+        content = response.read()
+        print("Body response:")
+        print("\t- type:", type(content))
+        print("\t- content:", content)
+        print("\t- utf8 content:", content.decode('utf-8'))
+except urllib.error.HTTPError as e:
+    print("HTTP Error:", e.code)
