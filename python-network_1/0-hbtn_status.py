532bd62b0beedde15019554c560d01c35f5bae1c
karizacharlotte <c.kariza@alustudent.com>
2024-03-24 17:43:45 +0000

    END

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-""" Python script that fetches https://intranet.hbtn.io/status """
-import urllib.request
+"""This script fetches "https://alu-intranet.hbtn.io/status
+   have to use urlib module/library"""
+
 if __name__ == "__main__":
-    url = 'https://intranet.hbtn.io/status'
-    if url.startsWith("https://"):
-        url = 'https://alu-intranet.hbtn.io/status'
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    req = urllib.request.Request(url, headers=headers)
-    with urllib.request.urlopen(req) as response:
-        html = response.read()
-        print('Body response:')
-        print('\t- type: {}'.format(type(html)))
-        print('\t- content: {}'.format(html))
-        print('\t- utf8 content: {}'.format(html.decode("utf-8")))
+    import urllib.request
+
+    with urllib.request.urlopen("https://alu-intranet.hbtn.io/status") as res:
+        content = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode("utf-8")))
