66adc9f89091ba4944127ab8645462b1e74728fb
karizacharlotte <c.kariza@alustudent.com>
2024-06-06 03:54:32 -0700

    End

--- javascript-warm_up/102-add_me_maybe.js
+++ javascript-warm_up/102-add_me_maybe.js
@@ -1,17 +1,4 @@
 #!/usr/bin/node
-const myObject = {
-  type: 'object',
-  value: 12
+exports.addMeMaybe = function (number, theFunction) {
+  theFunction(number + 1);
 };
-console.log(myObject);
-
-myObject.incr = function () {
-  this.value++;
-};
-
-myObject.incr();
-console.log(myObject);
-myObject.incr();
-console.log(myObject);
-myObject.incr();
-console.log(myObject);
